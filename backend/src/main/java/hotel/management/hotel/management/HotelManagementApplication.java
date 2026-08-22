49d7f806f165c1dc245e99669444b7d9c6480c70
LOKESH-sys365 <sidtyagim@gmail.com>
2026-08-22 16:29:37 +0530

    Fix DataSourceAutoConfiguration import path for Spring Boot 4.1.0

--- backend/src/main/java/hotel/management/hotel/management/HotelManagementApplication.java
+++ backend/src/main/java/hotel/management/hotel/management/HotelManagementApplication.java
@@ -2,8 +2,9 @@ package hotel.management.hotel.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {Database.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class HotelManagementApplication {
 
 	public static void main(String[] args) {
