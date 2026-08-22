40203d8829079ce8f2d80fd79f79d236bfa32961
LOKESH-sys365 <sidtyagim@gmail.com>
2026-08-22 13:33:16 +0530

    Fix: exclude DataSourceAutoConfiguration to use custom dual-datasource config

--- backend/src/main/java/hotel/management/hotel/management/HotelManagementApplication.java
+++ backend/src/main/java/hotel/management/hotel/management/HotelManagementApplication.java
@@ -3,7 +3,7 @@ package hotel.management.hotel.management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {Database.class})
 public class HotelManagementApplication {
 
 	public static void main(String[] args) {
